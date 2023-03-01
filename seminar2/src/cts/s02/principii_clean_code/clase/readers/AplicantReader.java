b136c704885b768493f5e4d92e6c9a094298abb9
daria-cirjau <dariaaax@yahoo.com>
2023-03-01 10:20:47 +0200

    sem 2 completat

---
+++ seminar2/src/cts/s02/principii_clean_code/clase/readers/AplicantReader.java
@@ -0,0 +1,2 @@
+package cts.s02.principii_clean_code.clase.readers;public interface ApplicantReader {
+}
---
+++ seminar2/src/cts/s02/principii_clean_code/clase/readers/StudentReader.java
@@ -0,0 +1,14 @@
+package cts.s02.principii_clean_code.clase.readers;
+
+import cts.s02.principii_clean_code.clase.Aplicant;
+
+import java.util.List;
+
+public class ReadStudent implements AplicantReader{
+
+    @Override
+    public List<Aplicant> readAplicant() {
+
+        return null;
+    }
+}
